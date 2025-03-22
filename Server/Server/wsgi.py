5908c7bd464c72fcedf643fa6634486ac2179522
Mohamed <mohammedmohamuud.01@gmail.com>
2025-03-22 23:22:53 +0300

    Update wsgi.py to include module-level docstring and correct settings module path

--- Server/Server/wsgi.py
+++ Server/Server/wsgi.py
@@ -1,11 +1,17 @@
-import os
-import sys
+"""
+WSGI config for Server project.
 
-# Add the 'Server' directory to the Python path
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Server.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Server.Server.settings')
+
 
 application = get_wsgi_application()
