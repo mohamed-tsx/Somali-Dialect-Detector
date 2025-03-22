51124818bdcea9396f544974ab9405d48bd8c9db
Mohamed <mohammedmohamuud.01@gmail.com>
2025-03-22 23:57:04 +0300

    Add django-heroku integration to settings.py for Heroku deployment

--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,4 +142,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Frontend URL during development
     # Add other allowed origins as needed
 ]
+
+django_heroku.settings(locals())
 
