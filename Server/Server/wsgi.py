5cdcc5f9970402303e87dac0565004eda4df7ae6
Mohamed <mohammedmohamuud.01@gmail.com>
2025-03-22 23:18:55 +0300

    Fix wsgi path and module loading

--- Server/Server/wsgi.py
+++ Server/Server/wsgi.py
@@ -1,17 +1,11 @@
-"""
-WSGI config for Server project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Add the 'Server' directory to the Python path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Server.Server.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Server.settings')
 
 application = get_wsgi_application()
