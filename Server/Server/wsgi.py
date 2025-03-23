84724704d4c4067292ccd887f6cf7ac96e4b5dea
Mohamed <mohammedmohamuud.01@gmail.com>
2025-03-23 16:20:17 +0300

    Fix import path for dialect_detection app and update WSGI configuration

--- Server/Server/wsgi.py
+++ Server/Server/wsgi.py
@@ -1,17 +1,10 @@
-"""
-WSGI config for Server project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Add the parent directory (which should be /app/Server) to the Python path
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Server.settings')
-
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Server.settings")
 application = get_wsgi_application()
