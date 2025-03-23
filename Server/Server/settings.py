636d1faa60c84c2b17650c0a05565b18d8394cf6
Mohamed <mohammedmohamuud.01@gmail.com>
2025-03-23 15:51:24 +0300

    Remove django-heroku integration from settings.py

--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,5 +142,4 @@ CORS_ALLOWED_ORIGINS = [
     # Add other allowed origins as needed
 ]
 
-django_heroku.settings(locals())
 
